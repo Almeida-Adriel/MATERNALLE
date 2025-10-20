695bcc11d860845fe7c9aab37c6a447f8739a981
Adriel Almeida Pereira Leite <adrielleite909@gmail.com>
2025-10-20 13:28:47 -0400

    rota usuarios/id/filhos adicionada

--- api/routes/usuarios.js
+++ api/routes/usuarios.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getAllUsuarios, getUsuario } from '../controllers/usuarios.js';
+import { getAllUsuarios, getUsuario, postFilhos } from '../controllers/usuarios.js';
 import authMiddleware from '../utils/middleaware.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleware, getAllUsuarios);
 router.get('/usuario', authMiddleware, getUsuario);
+router.post('/usuario/:userId/filhos', authMiddleware, postFilhos);
 
 export default router;
