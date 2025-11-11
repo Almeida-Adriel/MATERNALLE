74cae5f2310100e5b95192b5bea80af3cc6cfa35
Adriel Almeida Pereira Leite <adrielleite909@gmail.com>
2025-11-11 09:52:32 -0400

    concertos para deploy

--- app/src/utils/service/Service.js
+++ app/src/utils/service/Service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Auth from "./Auth";
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://maternalle.onrender.com/";
 const auth = new Auth();
 
 const handle401Error = (error) => {
